8eb3b78af8550bb370e010c6915b7890bdeebecb
Jonatan Jansson <social@dotmavriq.life>
2023-11-14 12:03:17 +0000

    Update coswift.php
    added Save button to save the Token entered by the user and changed Sync into Test API for now

--- coswift.php
+++ coswift.php
@@ -26,6 +26,20 @@ function coswift_admin_menu() {
 
 // Settings page content.
 function coswift_settings_page() {
+    // Check user capabilities
+    if (!current_user_can('manage_options')) {
+        return;
+    }
+
+    // Save settings if POST request is made
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['coswift_action']) && $_POST['coswift_action'] === 'save_settings') {
+        // Check if token is set in POST request
+        if (isset($_POST['coswift_api_token'])) {
+            // Save the token using update_option
+            update_option('coswift_api_token', sanitize_text_field($_POST['coswift_api_token']));
+            echo '<div id="setting-error-settings_updated" class="updated settings-error notice is-dismissible"><p><strong>Settings saved.</strong></p></div>';
+        }
+    }
     ?>
     <div class="wrap">
         <h2>CoSwift Settings</h2>
@@ -33,12 +47,16 @@ function coswift_settings_page() {
             <?php
                 settings_fields('coswift_options_group');
                 do_settings_sections('coswift-settings');
-                submit_button('Sync');
+                submit_button('Save Settings');
             ?>
+            <input type="hidden" name="coswift_action" value="save_settings">
+        </form>
+        <form method="post">
+            <input type="hidden" name="coswift_action" value="test_api">
+            <?php submit_button('Test API'); ?>
         </form>
         <?php
-            if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-                // Added styled div for JSON output
+            if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['coswift_action']) && $_POST['coswift_action'] === 'test_api') {
                 echo '<div class="json-output">';
                 echo fetch_coswift_job_listings();
                 echo '</div>';
@@ -46,16 +64,15 @@ function coswift_settings_page() {
         ?>
     </div>
     <style>
-        /* CSS for the JSON output box */
         .json-output {
-            max-height: 400px; /* Adjust height as needed */
-            max-width: 100%; /* Adjust width as needed */
-            overflow: auto; /* Adds scrollbar when content overflows */
-            background-color: #f9f9f9; /* Optional: background color */
-            border: 1px solid #ddd; /* Optional: border */
-            padding: 10px; /* Optional: inner spacing */
-            font-family: monospace; /* Optional: monospace font for better readability */
-            white-space: pre-wrap
+            max-height: 400px;
+            max-width: 100%;
+            overflow: auto;
+            background-color: #f9f9f9;
+            border: 1px solid #ddd;
+            padding: 10px;
+            font-family: monospace;
+            white-space: pre-wrap;
         }
     </style>
     <?php
