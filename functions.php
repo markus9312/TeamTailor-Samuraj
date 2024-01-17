f5af37aeb020bd8fac43b317bf3cfcbb924de58a
dotMavriQ <dotmavriq@dotmavriq.life>
2024-01-17 00:23:52 +0000

    added ACF override and Register with Elementor to CoSwift.php

--- functions.php
+++
@@ -1,6 +0,0 @@
-add_action('admin_head', function () {
-    global $post_type;
-    if ('coswift_jobs' == $post_type) {
-        add_filter('acf/settings/remove_wp_meta_box', '__return_false');
-    }
-});
